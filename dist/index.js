7608a767540d2cb0fd200271f20300e8d5a39a67
chjuca <chjuca@utpl.edu.ec>
2019-11-26 18:36:18 -0500

    Agregando dist

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,16 @@
 "use strict";
 
-var hello = "hello word";
+var _app = _interopRequireDefault(require("./app"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+require('./config/config');
+
+function main() {
+  _app["default"].listen(process.env.PORT);
+
+  console.log('Server on port:', process.env.PORT);
+}
+
+;
+main();
