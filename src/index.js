366039f6f5100ed8ee96b13b97ad88a7c5807857
chjuca <chjuca@utpl.edu.ec>
2019-11-26 20:21:58 -0500

    Agregando babel-polyfill

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app';
 
 function main(){
     app.listen(process.env.PORT);
-    console.log('Server on port:', process.env.PORT)
+    console.log('Server on port:', process.env.PORT) 
 };
 
 main();
